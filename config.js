92c271fddda6061994242720fcaf5c35956b5dfe
Dominik Soboń <dominik.sobon@gmail.com>
2018-09-20 15:55:57 +0200

    Change js files

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'GOOGLE_CLIENT_ID': '896265619233-48l6m4j23shffqpfilaaso21vs4qutpd.apps.googleusercontent.com',
+  'GOOGLE_CLIENT_SECRET': 'xphnbE-t4nDhjbpAlTK3hLu2',
+  "CALLBACK_URL": 'http://localhost:3000/auth/google/callback'
+};
